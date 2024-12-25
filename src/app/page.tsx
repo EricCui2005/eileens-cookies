dcf10a2d5c6d57dc78b669478abbc6f5f1a8be6e
ericcui <ericcui2005@gmail.com>
2024-12-24 23:15:18 -0500

    Fixing hopefully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Landing from './components/Landing';
-import About from './components/About';
-import Contact from './components/Contact';
-import Gallery from './components/Gallery';
+import Landing from '@/components/Landing';
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Gallery from '@/components/Gallery';
 
 export default function Home() {
   return (
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -5,7 +5,7 @@ export default function Landing() {
   return (
     <>
       <div id="landing-div" className="h-[100vh] w-[100vw]">
-        <div className="bg-darkBrown flex h-[10vh] items-center justify-center">
+        <div className="flex h-[10vh] items-center justify-center bg-darkBrown">
           <div
             id="navigation-links"
             className="justify-items flex items-center gap-24 text-[1.1vw]"
@@ -18,7 +18,7 @@ export default function Landing() {
         </div>
         <div
           id="big-text-div"
-          className="bg-lightBrown flex h-[90vh] w-[100vw] items-center justify-center"
+          className="flex h-[90vh] w-[100vw] items-center justify-center bg-lightBrown"
         >
           <h1 className="text-[16vw] text-white">
             <VaraText text="Eileen's Cookies" />
