11b3fa9b2406ab1010ad8048714b4ef37954c0ec
ericcui <ericcui2005@gmail.com>
2024-12-24 23:28:28 -0500

    Fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Landing from '@/components/Landing';
-import About from '@/components/About';
-import Contact from '@/components/Contact';
-import Gallery from '@/components/Gallery';
+import Landing from './components/landing';
+import About from './components/about';
+import Contact from './components/contact';
+import Gallery from './components/gallery';
 
 export default function Home() {
   return (
